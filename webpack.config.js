062d70bbbea169d0c31216ce4d6d43c19a029787
Yassine AITALLA <yaitalla@e3r4p20.42.fr>
2019-05-20 14:34:58 +0200

    boilerplate updated

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/client/index.js',
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        loader: 'url-loader',
+        options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[hash]-[name].[ext]'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query:{
+          presets: ["es2015", "react", "stage-0"]
+        }
+    }]
+  }
+};
