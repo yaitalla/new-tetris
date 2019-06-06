fdb401e9d000ad9687fa72411cb4d393b9f59e0e
yaitalla <yaitalla.dev@gmail.com>
2019-06-06 08:41:27 +0200

    morning

--- client/src/actions/down.js
+++ client/src/actions/down.js
@@ -3,10 +3,7 @@ import gridCopy from '../config/misc/gridCopy';
 import { DOWN, ADD_TETRI, GAME_OVER} from '../config/constants';
 import nextShape from '../config/misc/applyNewShape';
 
-const addTetriminos = (field) => {
-    const state = store.getState();
-    const i = state.shapeIndex;
-    const room = state.rooms[state.actualRoom]
+const addTetriminos = (field, i, room) => {
     for (let i=0; i<20; i++) {
         for (let j=0; j<10; j++) {
             if (field[i][j] == 2 ) {
@@ -19,13 +16,14 @@ const addTetriminos = (field) => {
 
 
 const down = () => {
-    const field = store.getState().field;
+    const state = store.getState();
+    const field = state.field;
     const ret = gridCopy(field);
     for (let i=0; i<20; i++) {
         for (let j=0; j<10; j++) {
             if (field[i][j] == 2 ) {
                 if (i > 18 || field[i+1][j] == 1) { //le sol
-                    return (addTetriminos(field))
+                    return (addTetriminos(field, state.shapeIndex, state.rooms[state.actualRoom]))
                 }
                 ret[i+1][j] = 2;
             }
